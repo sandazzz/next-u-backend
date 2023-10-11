9f6e6a10b8c500afc711780f1f2b8b4e1a23ba1d
Alejandro Oviedo <alejandro.oviedo.g@gmail.com>
2023-10-11 11:29:44 +0200

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -16,15 +16,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    connection: process.env.PG_CONNECTION_STRING,
     migrations: {
       tableName: 'knex_migrations'
     }
